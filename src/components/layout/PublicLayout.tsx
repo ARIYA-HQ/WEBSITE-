cd848468dc688664907441cfbd7762fc81a6624e
AriyaHQWeb User <user@example.com>
2026-03-17 13:22:58 +0100

    fixed overflow

--- src/components/layout/PublicLayout.tsx
+++ src/components/layout/PublicLayout.tsx
@@ -6,7 +6,7 @@ import CookieBanner from '../common/CookieBanner';
 
 export default function PublicLayout() {
     return (
-        <div className="min-h-screen bg-beige-100 dark:bg-gray-950 dark:text-gray-100 font-sans selection:bg-primary-600 selection:text-white transition-colors duration-300 flex flex-col">
+        <div className="min-h-screen overflow-x-hidden bg-beige-100 dark:bg-gray-950 dark:text-gray-100 font-sans selection:bg-primary-600 selection:text-white transition-colors duration-300 flex flex-col">
             <Header />
             <main>
                 <Outlet />
